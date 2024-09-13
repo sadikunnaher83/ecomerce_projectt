f90233ea164a270685f5c9aeb38cc9e149cbf8d0
Sadikun Naher <152975399+sadikunnaher83@users.noreply.github.com>
2024-09-14 01:36:35 +0600

    template add

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get('/', [HomeController::class, 'home']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
